f3a3c0316113ed71fbda66c3a780452a894ba6a4
Eduardo Henrique Dalmolin Corrêa <eduardo.correa@skyinformatica.com.br>
2024-12-20 13:54:50 -0300

    #7 - Criação do DTO ProjetoAlterado.cs

---
+++ src/Ferramentas.Cli/Domínio/ResumirPr/Dtos/ProjetoAlterado.cs
@@ -0,0 +1,8 @@
+namespace Ferramentas.Cli.Domínio.ResumirPr.Dtos;
+
+public class ProjetoAlterado
+{
+    public string NomeDoProjeto { get; set; } = string.Empty;
+    public TipoDoProjeto TipoDoProjeto { get; set; }
+    public int QuantidadeDeAlterações { get; set; } = 1;
+}
