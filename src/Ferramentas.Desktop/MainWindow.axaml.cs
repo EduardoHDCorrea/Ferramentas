5897f56dc643a0302aaeeac4d04a76659ad84305
Eduardo Henrique Dalmolin Corrêa <eduardo.correa@skyinformatica.com.br>
2024-12-23 14:33:25 -0300

    Criação do projeto desktop para testar fora de hora.

---
+++ src/Ferramentas.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Ferramentas.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Ferramentas.Desktop/Models/Enumerados/TipoDoProjeto.cs
@@ -0,0 +1,8 @@
+namespace Ferramentas.Desktop.Models.Enumerados;
+
+public enum TipoDoProjeto
+{
+    Fonte,
+    Testes,
+    Pacote
+}
